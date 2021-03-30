cd8f20bd18a50bd44ebb3e95d119eaa54e63411b
dcabral05 <75283200+dcabral05@users.noreply.github.com>
2021-03-30 16:14:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
     <html lang="es">
         <head>
             <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
             <title>Hola Mundo!</title>
         </head>
         <body>
